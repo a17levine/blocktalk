288876000530b52be6c7ad4b32ac65203e06f662
MakerSquare Student <student@instructor-3.att.net>
2013-07-23 14:44:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocktalk::Application.config.secret_token = '3cd7c90e1c4c6931b572514f22687c89f92a094d135eccdc8be741ffab9dbc2efea2b3ee2c26c3c2767eb204da0c985646532f9e87d6cf0b402f6a6b3284cec1'
