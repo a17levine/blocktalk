8e6b3e9623c882e41d32f5094b4415d0fa0f2581
Alex Levine <alexlevine@Alexs-MacBook-Air.local>
2013-07-24 11:18:38 -0500

    added migrations for users, timeblocks, and meetings

--- db/migrate/20130723231009_create_meetings.rb
+++ db/migrate/20130723231009_create_meetings.rb
@@ -1,7 +1,9 @@
 class CreateMeetings < ActiveRecord::Migration
   def change
     create_table :meetings do |t|
-
+    	t.integer			:host
+    	t.integer			:guest
+    	t.integer			:agreed_time_block
       t.timestamps
     end
   end
--- db/migrate/20130723231028_create_time_blocks.rb
+++ db/migrate/20130723231028_create_time_blocks.rb
@@ -1,7 +1,8 @@
 class CreateTimeBlocks < ActiveRecord::Migration
   def change
     create_table :time_blocks do |t|
-
+    	t.datetime		:start_time
+    	t.integer 		:meeting_id
       t.timestamps
     end
   end
