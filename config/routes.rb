542e092b321ca0c0a8e72e83873e9eab3a21f941
Eric Levine <djlevine@gmail.com>
2013-07-24 09:22:42 -0500

    Changed root to Meetings controller

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Blocktalk::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'session#index'
+  root :to => 'meetings#index'
 
   # See how all your routes lay out with "rake routes"
 
