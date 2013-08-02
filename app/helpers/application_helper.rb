8438b2aa07da4e1a37ec1447027c155c7af58d80
Eric Levine <djlevine@gmail.com>
2013-08-02 17:30:14 -0500

    took out include tree, added stylesheet includes, stylized show calendar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,7 @@ module ApplicationHelper
     content_for(:head) { javascript_include_tag(*files) }
   end
 
+  def stylesheet(*files)
+    content_for(:head) { stylesheet_link_tag(*files)}
+  end
 end
