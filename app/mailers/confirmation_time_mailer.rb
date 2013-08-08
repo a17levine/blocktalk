443a5f9dfed4242c36ac54d92d28bbbb6c7853ce
Eric Levine <djlevine@gmail.com>
2013-08-08 16:21:51 -0500

    link lightbox works, ajax repeats

--- app/mailers/confirmation_time_mailer.rb
+++ app/mailers/confirmation_time_mailer.rb
@@ -2,7 +2,7 @@ class ConfirmationTimeMailer < ActionMailer::Base
   
   default from: "mksblocktalk@gmail.com"
 
-  def confirmation_time_email(email1)
+  def confirmation_time_email(email)
     @email = email
     mail(to: "User Name <#{@email}>", subject: "Blocktalk Confirmation")
   end
