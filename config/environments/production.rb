6270c47433dd94aba5852c6bffc84e737ef650b1
Alex Levine <a17levine@gmail.com>
2013-08-14 15:09:24 -0500

    reconfigured environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ Blocktalk::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
