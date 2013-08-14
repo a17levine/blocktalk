b0b81f77e2fa013dfd3e3f6e0ac27abd894ac9ad
Silas Rioux <silasrioux@unknown1ce62ba06433.att.net>
2013-08-14 09:58:17 -0500

    fix lightbox button

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -65,8 +65,9 @@ $(document).ready(function(){
   $(".fancybox").fancybox({
           afterLoad   : function() {
             this.inner.prepend( "<div class='row'><div class='small-10 small-offset-2 columns'><h2>Share this link with your friend:</h2></div></div>" );
-            this.content = "<div class='row'><div class='small-10 small-offset-1 columns'><textarea name='box-content' font-size: 48px; id='zclip' rows='2' cols='60'>" + meetingLink + '</textarea>' + '<br />' + 
-            "<a href='" + meetingLink + "'</div></div><div class='row'><div class='button small-4 small-offset-4 columns'>click to proceed</div></a></div>";
+
+            this.content = "<div class='row'><div class='small-10 small-offset-1 columns'><textarea name='box-content' font-size: 48px; id='zclip' rows='2' cols='60'>" + meetingLink + '</textarea></div></div>' + '<br />' + 
+            "<div class='row'><a class='button small-4 small-offset-4' href='" + meetingLink + "'>click to proceed</a></div>";
              
             // Copy Button for later:      "<p><input type='button' data-clipboard-target='fetext' id='copy' class='button' name='copy' value='copy' data-clipboard-text='Test'/></p>"
             // Document Icon for later:    "<i class='icon-file-text-alt icon-4x'></i>"
