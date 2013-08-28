daae2335c92c86ec41aab639cd7625136875b8fb
Alex Levine <a17levine@gmail.com>
2013-08-27 19:19:09 -0500

    now ical events are attachments and the HTML renders

--- app/mailers/confirmation_time_mailer.rb
+++ app/mailers/confirmation_time_mailer.rb
@@ -1,37 +1,38 @@
 class ConfirmationTimeMailer < ActionMailer::Base
   
   # default from: "mksblocktalk@gmail.com"
-  default from: "blocktalk.io"
+  # default from: "blocktalk.io"
 
   def confirmation_time_email(meeting)
+  	@meeting = meeting
   	@host = meeting.host
   	@guest = meeting.guest
     @subject_line = "scheduled: #{@guest.email_username} + #{@host.email_username} - blocktalk"
     # binding.pry
     @agreed_time = meeting.agreed_time_block.start_time.to_datetime
-    mail(to: "<#{@host.email}>, <#{@guest.email}>", subject: @subject_line) do |format|
-       format.ics {
-	       ical = Icalendar::Calendar.new
-	       e = Icalendar::Event.new
-	       e.start = (@agreed_time).utc
-	       e.start.icalendar_tzid="UTC" # set timezone as "UTC"
-	       e.end = (@agreed_time + 60.minutes).utc
-	       e.end.icalendar_tzid="UTC"
-	       e.organizer "#{@host.email}"
-	       # i dont think the following function works
-	       # trying to add the guests on to the ical email
-	       # not easy apparently
-	       meeting.users.each do |user|
-	       	e.add_attendee "mailto:#{user.email}"
-	       end
+    attachments['event.ics'] = {:mime_type => 'text/calendar', :content => create_ical }
+    mail(from: "blocktalk.io", to: "<#{@host.email}>, <#{@guest.email}>", subject: @subject_line)
+  end
 
-	       e.uid "MeetingRequest #{meeting.token}"
-	       e.summary @subject_line
-	       e.description "This meeting was created with blocktalk"
-	       ical.add_event(e)
-	       ical.publish
-	       render :text => ical.to_ical, :layout => false
-      }
-  	end
+  def create_ical
+   ical = Icalendar::Calendar.new
+   e = Icalendar::Event.new
+   e.start = (@agreed_time).utc
+   e.start.icalendar_tzid="UTC" # set timezone as "UTC"
+   e.end = (@agreed_time + 30.minutes).utc
+   e.end.icalendar_tzid="UTC"
+   e.organizer "#{@host.email}"
+   # i dont think the following function works
+   # trying to add the guests on to the ical email
+   # not easy apparently
+   @meeting.users.each do |user|
+   	e.add_attendee "mailto:#{user.email}"
+   end
+   e.uid "MeetingRequest #{@meeting.token}"
+   e.summary @subject_line
+   e.description "This meeting was created with blocktalk"
+   ical.add_event(e)
+   ical.publish
+   ical.to_ical
   end
 end
