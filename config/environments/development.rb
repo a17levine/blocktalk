b181afcd4bbc89ee878d2e328586383c1fcb92e9
Alex Levine <a17levine@gmail.com>
2013-09-03 15:49:52 -0500

    proving i own the domain to sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,10 +37,10 @@ Blocktalk::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 
