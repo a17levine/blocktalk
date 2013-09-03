fa6b0d6b7d71f0b1a68912bc1165e89186cb21d8
Alex Levine <a17levine@gmail.com>
2013-09-03 15:29:04 -0500

    fixed syntax

--- app/assets/javascripts/_display_meeting.js
+++ app/assets/javascripts/_display_meeting.js
@@ -1,18 +1,18 @@
 function meetingTimeInWords() {
-	  //figure out if the time speaking is before now or after
-	  if (meetingTimeMoment > moment()){
-	    //if in the future display 'will be speaking in'
-	    //insert the moment words into the right div
-	    var futureText = "will meet " + meetingTimeMoment.fromNow().replace('in ','') + " from now";
-	    $('.speakingWords').first().text(futureText)
-	  } 
-	  else {
-	    //if in the past display 'met'
-	    //insert moment words into the right div
-	    var pastText = "met " + meetingTimeMoment.fromNow();
-	    $('.speakingWords').first().text(pastText);
-	  }
+	//figure out if the time speaking is before now or after
+  if (meetingTimeMoment > moment()){
+    //if in the future display 'will be speaking in'
+    //insert the moment words into the right div
+    var futureText = "will meet " + meetingTimeMoment.fromNow().replace('in ','') + " from now";
+    $('.speakingWords').first().text(futureText)
+  } 
+  else {
+    //if in the past display 'met'
+    //insert moment words into the right div
+    var pastText = "met " + meetingTimeMoment.fromNow();
+    $('.speakingWords').first().text(pastText);
   }
+}
 
 
 $(document).ready(function(){
